770c9e69065c9d8325481dd681457b7e3553ca0e
Gabriel Ferreira <ferreiragabriel2612@gmail.com>
2023-04-12 16:11:28 -0300

    Adição do lombok

--- Escola/src/main/java/br/com/gabrielferreira/model/Peso.java
+++ Escola/src/main/java/br/com/gabrielferreira/model/Peso.java
@@ -1,60 +1,24 @@
 package br.com.gabrielferreira.model;
 
+import lombok.*;
+
 import java.io.Serial;
 import java.io.Serializable;
-import java.util.Objects;
 import java.util.UUID;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Generated
+@ToString
 public class Peso implements Serializable {
 
     @Serial
     private static final long serialVersionUID = -1998463533526511852L;
 
+    @EqualsAndHashCode.Include
     private UUID id;
 
     private Integer valor;
-
-    public Peso(){}
-
-    public Peso(UUID id, Integer valor) {
-        this.id = id;
-        this.valor = valor;
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public Integer getValor() {
-        return valor;
-    }
-
-    public void setValor(Integer valor) {
-        this.valor = valor;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Peso peso = (Peso) o;
-        return id.equals(peso.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
-
-    @Override
-    public String toString() {
-        return "Peso{" +
-                "id=" + id +
-                ", valor=" + valor +
-                '}';
-    }
 }
--- Escola/src/main/java/br/com/gabrielferreira/model/Prova.java
+++ Escola/src/main/java/br/com/gabrielferreira/model/Prova.java
@@ -1,15 +1,23 @@
 package br.com.gabrielferreira.model;
 
+import lombok.*;
+
 import java.io.Serial;
 import java.io.Serializable;
-import java.util.Objects;
 import java.util.UUID;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Generated
+@ToString
 public class Prova implements Serializable {
 
     @Serial
     private static final long serialVersionUID = 9162731473093293178L;
 
+    @EqualsAndHashCode.Include
     private UUID id;
 
     private String nome;
@@ -17,68 +25,4 @@ public class Prova implements Serializable {
     private Double nota;
 
     private Peso peso;
-
-    public Prova(){}
-
-    public Prova(UUID id, String nome ,Double nota, Peso peso) {
-        this.id = id;
-        this.nome = nome;
-        this.nota = nota;
-        this.peso = peso;
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public Double getNota() {
-        return nota;
-    }
-
-    public void setNota(Double nota) {
-        this.nota = nota;
-    }
-
-    public Peso getPeso() {
-        return peso;
-    }
-
-    public void setPeso(Peso peso) {
-        this.peso = peso;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Prova prova = (Prova) o;
-        return id.equals(prova.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
-
-    @Override
-    public String toString() {
-        return "Prova{" +
-                "id=" + id +
-                ", nome='" + nome + '\'' +
-                ", nota=" + nota +
-                ", peso=" + peso +
-                '}';
-    }
 }
