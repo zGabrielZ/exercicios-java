c8f114af47366cb5b5a3d46d2410a3d27c480450
Gabriel Ferreira <ferreiragabriel2612@gmail.com>
2023-04-12 19:48:17 -0300

    Ajustes no ex 04

--- Numeros/src/main/java/br/com/gabrielferreira/aplicacao/Ex04.java
+++ Numeros/src/main/java/br/com/gabrielferreira/aplicacao/Ex04.java
@@ -4,7 +4,8 @@ public class Ex04 {
 
     public static void main(String[] args) {
         for (int i = 0; i <= 10; i++){
-            System.out.println(String.format("%s X %s = %s", 9, i, 9 * i));
+            String formula = String.format("%s X %s = %s", 9, i, 9 * i);
+            System.out.println(formula);
         }
     }
 }
