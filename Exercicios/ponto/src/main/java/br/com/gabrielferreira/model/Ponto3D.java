866e9f78e80ab4bdf9bbc32ef4bb412a4dfa007f
Gabriel Ferreira <ferreiragabriel2612@gmail.com>
2023-05-08 20:58:26 -0300

    Ajustes ponto

--- Exercicios/ponto/src/main/java/br/com/gabrielferreira/model/Ponto3D.java
+++ Exercicios/ponto/src/main/java/br/com/gabrielferreira/model/Ponto3D.java
@@ -3,6 +3,7 @@ import lombok.EqualsAndHashCode;
 import lombok.Generated;
 import lombok.Getter;
 
+import java.io.Serial;
 import java.util.UUID;
 
 @Getter
@@ -10,6 +11,8 @@ import java.util.UUID;
 @Generated
 public class Ponto3D extends Ponto2D {
 
+    @Serial
+    private static final long serialVersionUID = -4540216104680662311L;
     private final Double coordZ;
 
     public Ponto3D(UUID id, Double coordX, Double coordY, Double coordZ) {
