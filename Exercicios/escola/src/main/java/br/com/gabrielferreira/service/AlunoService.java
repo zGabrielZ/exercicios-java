fb8c7067e754644de88072757e9aaddc1b6349eb
Gabriel Ferreira <ferreiragabriel2612@gmail.com>
2023-05-18 22:23:11 -0300

    Ajustes no exercício escola

--- Exercicios/escola/src/main/java/br/com/gabrielferreira/service/AlunoService.java
+++ Exercicios/escola/src/main/java/br/com/gabrielferreira/service/AlunoService.java
@@ -1,8 +1,9 @@
 package br.com.gabrielferreira.service;
 
-import br.com.gabrielferreira.exception.RegraDeNegocioException;
 import br.com.gabrielferreira.model.Aluno;
 import br.com.gabrielferreira.model.Prova;
+
+import static br.com.gabrielferreira.validate.ValidarAluno.*;
 
 import java.io.Serial;
 import java.io.Serializable;
@@ -36,7 +37,7 @@ public class AlunoService implements Serializable {
                 somaPesos += prova.getPeso().getValor();
             }
 
-            validarSomaPesos(somaPesos);
+            validarSomaPeso(somaPesos);
 
             BigDecimal mediaProva = somaProvas.divide(BigDecimal.valueOf(somaPesos), RoundingMode.HALF_EVEN);
             sb.append("Aluno : ").append(aluno.getNome()).append(", Média : ").append(mediaProva).append("\n");
@@ -47,47 +48,24 @@ public class AlunoService implements Serializable {
     }
 
     private void validarAlunos(List<Aluno> alunos){
-        if(alunos.isEmpty()){
-            throw new RegraDeNegocioException("É necessário informar pelo menos um aluno");
-        }
+        validarLista(alunos, "É necessário informar pelo menos um aluno");
     }
 
     private void validarNome(String nome){
-        if(nome == null || nome.isBlank()){
-            throw new RegraDeNegocioException("É necessário informar o nome do aluno");
-        }
+        validarString(nome, "É necessário informar o nome do aluno");
     }
 
     private void validarNomeProva(String nome, String nomeAluno){
-        if(nome == null || nome.isBlank()){
-            throw new RegraDeNegocioException(String.format("É necessário informar o nome da prova do aluno %s", nomeAluno));
-        }
+        validarString(nome, String.format("É necessário informar o nome da prova do aluno %s", nomeAluno));
     }
 
     private void validarNotaProva(BigDecimal nota, String nomeAluno){
-        if(nota == null){
-            throw new RegraDeNegocioException(String.format("É necessário informar a nota do aluno %s", nomeAluno));
-        }
-
-        if(!(nota.compareTo(BigDecimal.ZERO) >= 0 && nota.compareTo(BigDecimal.TEN) <= 0)){
-            throw new RegraDeNegocioException(String.format("A nota do aluno %s tem que ser de 0.0 até 10.0", nomeAluno));
-        }
-
+        validarBigDecimal(nota, String.format("É necessário informar a nota do aluno %s", nomeAluno));
+        validarNotaAluno(nota, nomeAluno);
     }
 
     private void validarPeso(Integer peso, String nomeAluno){
-        if(peso == null){
-            throw new RegraDeNegocioException(String.format("É necessário informar o peso da nota do aluno %s", nomeAluno));
-        }
-
-        if(!(peso >= 1 && peso <= 5)){
-            throw new RegraDeNegocioException(String.format("O peso da nota do aluno %s tem que ser de 1 até 5", nomeAluno));
-        }
-    }
-
-    private void validarSomaPesos(Integer somaPesos){
-        if(somaPesos > 10){
-            throw new RegraDeNegocioException("A nota do peso não pode ultrapassar de 10");
-        }
+        validarInteger(peso, String.format("É necessário informar o peso da nota do aluno %s", nomeAluno));
+        validarPeriodoPeso(peso, nomeAluno);
     }
 }
