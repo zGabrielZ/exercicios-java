740a86bb9bc97ae8bb4c423040f858142c6fc1fc
Gabriel Ferreira <ferreiragabriel2612@gmail.com>
2023-05-18 21:39:56 -0300

    Ajustes no exercício conta bancária

--- Exercicios/conta-bancaria/src/main/java/br/com/gabrielferreira/model/ContaBancaria.java
+++ Exercicios/conta-bancaria/src/main/java/br/com/gabrielferreira/model/ContaBancaria.java
@@ -1,13 +1,13 @@
 package br.com.gabrielferreira.model;
 
-import br.com.gabrielferreira.exception.SaldoInsuficienteException;
-import br.com.gabrielferreira.exception.ValorInvalidoException;
 import lombok.*;
 
 import java.io.Serial;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.UUID;
+
+import static br.com.gabrielferreira.validate.ValidarConta.*;
 
 @NoArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
@@ -26,38 +26,20 @@ public abstract class ContaBancaria implements Serializable {
     private BigDecimal saldo = BigDecimal.ZERO;
 
     public void depositar(BigDecimal saldo){
-        if(saldo == null){
-            throw new ValorInvalidoException("É necessário informar o valor para depositar");
-        }
-
-        if(saldo.compareTo(BigDecimal.ZERO) <= 0){
-            throw new ValorInvalidoException("Não é possível depositar com o valor zerado ou negativo");
-        }
-
+        validarSaldoInformado(saldo, "É necessário informar o valor para depositar");
+        validarSaldoNegativoOuZerado(saldo, "Não é possível depositar com o valor zerado ou negativo");
         this.saldo = this.saldo.add(saldo);
     }
 
     public void sacar(BigDecimal saldo){
-        if(saldo == null){
-            throw new ValorInvalidoException("É necessário informar o valor para sacar");
-        }
-
-        if(saldo.compareTo(BigDecimal.ZERO) <= 0){
-            throw new ValorInvalidoException("Não é possível sacar com o valor zerado ou negativo");
-        }
-
-        if(saldo.compareTo(this.getSaldo()) > 0){
-            throw new SaldoInsuficienteException("Não é possível sacar pois o valor final é menor que o valor informado");
-        }
-
+        validarSaldoInformado(saldo, "É necessário informar o valor para sacar");
+        validarSaldoNegativoOuZerado(saldo, "Não é possível sacar com o valor zerado ou negativo");
+        validarSaldoTotalMenorQueSaldoInformado(this.getSaldo(), saldo, "Não é possível sacar pois o valor final é menor que o valor informado");
         this.saldo = this.saldo.subtract(saldo);
     }
 
     public void transferir(BigDecimal saldoParaTransferir, ContaBancaria contaBancariaDestino){
-        if(saldoParaTransferir == null){
-            throw new ValorInvalidoException("É necessário informar o valor do saldo para transferir");
-        }
-
+        validarSaldoInformado(saldoParaTransferir, "É necessário informar o valor do saldo para transferir");
         sacar(saldoParaTransferir);
         contaBancariaDestino.depositar(saldoParaTransferir);
     }
