7a2ad5cc6a0784621b944de8b5a4e656554ebafe
Gabriel Ferreira <ferreiragabriel2612@gmail.com>
2023-05-20 15:50:20 -0300

    Ajustes testes unitários conta bancaria e diagrama de classe

--- Exercicios/conta-bancaria/src/test/java/br/com/gabrielferreira/model/ContaInvestimentoTest.java
+++ Exercicios/conta-bancaria/src/test/java/br/com/gabrielferreira/model/ContaInvestimentoTest.java
@@ -8,6 +8,7 @@ import java.math.RoundingMode;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class ContaInvestimentoTest {
 
@@ -23,5 +24,32 @@ class ContaInvestimentoTest {
         BigDecimal resultado = contaBancariaOrigem.calcularSaldoFinal();
 
         assertEquals(BigDecimal.valueOf(225.0), resultado.setScale(1, RoundingMode.HALF_EVEN));
+    }
+
+    @Test
+    @DisplayName("Deve comparar conta investimento quando não forem iguais")
+    void deveCompararContaInvestimentoNaoIguais(){
+        ContaBancaria contaBancaria1 = new ContaInvestimento();
+        contaBancaria1.setId(UUID.randomUUID());
+
+        ContaBancaria contaBancaria2 = new ContaInvestimento();
+        contaBancaria2.setId(UUID.randomUUID());
+
+        assertNotEquals(contaBancaria1, contaBancaria2);
+        assertNotEquals(contaBancaria1.hashCode(), contaBancaria2.hashCode());
+    }
+
+    @Test
+    @DisplayName("Deve comparar conta investimento quando forem iguais")
+    void deveCompararContaInvestimentoIguais(){
+        UUID id = UUID.randomUUID();
+        ContaBancaria contaBancaria1 = new ContaInvestimento();
+        contaBancaria1.setId(id);
+
+        ContaBancaria contaBancaria2 = new ContaInvestimento();
+        contaBancaria2.setId(id);
+
+        assertEquals(contaBancaria1, contaBancaria2);
+        assertEquals(contaBancaria1.hashCode(), contaBancaria2.hashCode());
     }
 }
