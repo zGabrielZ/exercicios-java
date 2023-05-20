0219b77bc29e18971c8b5a20f674a52775c7caea
Gabriel Ferreira <ferreiragabriel2612@gmail.com>
2023-05-20 20:27:08 -0300

    Ajustes testes unitários ponto e diagrama de classes

--- Exercicios/ponto/src/test/java/br/com/gabrielferreira/model/Ponto2DTest.java
+++ Exercicios/ponto/src/test/java/br/com/gabrielferreira/model/Ponto2DTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.math.BigDecimal;
 import java.util.UUID;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class Ponto2DTest {
 
@@ -18,6 +19,29 @@ class Ponto2DTest {
 
         String resultadoEsperado = "Id : " + ponto2D.getId() + ", coordX : " + ponto2D.getCoordX() + ", coordY : " + ponto2D.getCoordY();
         assertEquals(resultadoEsperado, resultado);
+    }
+
+    @Test
+    @DisplayName("Deve comparar Ponto2D quando não forem iguais")
+    void deveCompararPonto2DNaoIguais(){
+        Ponto2D ponto1 = new Ponto2D(UUID.randomUUID(), BigDecimal.valueOf(2.5), BigDecimal.valueOf(5.4));
+        Ponto2D ponto2 = new Ponto2D(UUID.randomUUID(), BigDecimal.valueOf(2.6), BigDecimal.valueOf(5.7));
+
+        assertNotEquals(ponto1, ponto2);
+        assertNotEquals(ponto1.hashCode(), ponto2.hashCode());
+    }
+
+    @Test
+    @DisplayName("Deve comparar Ponto2D quando forem iguais")
+    void deveCompararPonto2DIguais(){
+        UUID id = UUID.randomUUID();
+        BigDecimal x = BigDecimal.ONE;
+        BigDecimal y = BigDecimal.TEN;
+        Ponto2D ponto1 = new Ponto2D(id, x, y);
+        Ponto2D ponto2 = new Ponto2D(id, x, y);
+
+        assertEquals(ponto1, ponto2);
+        assertEquals(ponto1.hashCode(), ponto2.hashCode());
     }
 
 }
--- Exercicios/ponto/src/test/java/br/com/gabrielferreira/model/Ponto3DTest.java
+++ Exercicios/ponto/src/test/java/br/com/gabrielferreira/model/Ponto3DTest.java
@@ -7,6 +7,7 @@ import java.math.BigDecimal;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class Ponto3DTest {
 
@@ -19,6 +20,30 @@ class Ponto3DTest {
 
         String resultadoEsperado = "Id : " + ponto3D.getId() + ", coordX : " + ponto3D.getCoordX() + ", coordY : " + ponto3D.getCoordY() + ", coordZ : " + ponto3D.getCoordZ();
         assertEquals(resultadoEsperado, resultado);
+    }
+
+    @Test
+    @DisplayName("Deve comparar Ponto3D quando não forem iguais")
+    void deveCompararPonto3DNaoIguais(){
+        Ponto3D ponto1 = new Ponto3D(UUID.randomUUID(), BigDecimal.valueOf(2.5), BigDecimal.valueOf(5.4), BigDecimal.valueOf(10.0));
+        Ponto3D ponto2 = new Ponto3D(UUID.randomUUID(), BigDecimal.valueOf(2.6), BigDecimal.valueOf(5.7), BigDecimal.valueOf(30.0));
+
+        assertNotEquals(ponto1, ponto2);
+        assertNotEquals(ponto1.hashCode(), ponto2.hashCode());
+    }
+
+    @Test
+    @DisplayName("Deve comparar Ponto3D quando forem iguais")
+    void deveCompararPonto3DIguais(){
+        UUID id = UUID.randomUUID();
+        BigDecimal x = BigDecimal.ONE;
+        BigDecimal y = BigDecimal.TEN;
+        BigDecimal z = BigDecimal.ZERO;
+        Ponto3D ponto1 = new Ponto3D(id, x, y, z);
+        Ponto3D ponto2 = new Ponto3D(id, x, y, z);
+
+        assertEquals(ponto1, ponto2);
+        assertEquals(ponto1.hashCode(), ponto2.hashCode());
     }
 
 }
