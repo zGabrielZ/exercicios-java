6c9323b9924ac462f86380a027fcdcef9237149f
Gabriel Ferreira <ferreiragabriel2612@gmail.com>
2023-05-26 16:08:05 -0300

    Ponto console app

---
+++ Exercicios/console-app/src/main/java/br/com/gabrielferreira/main/AplicacaoPonto.java
@@ -0,0 +1,23 @@
+package br.com.gabrielferreira.main;
+
+import br.com.gabrielferreira.model.Ponto2D;
+import br.com.gabrielferreira.model.Ponto3D;
+
+import static br.com.gabrielferreira.utils.CalculoUtils.*;
+
+import java.util.UUID;
+
+public class AplicacaoPonto {
+
+    public static void main(String[] args) {
+        System.out.println("Iniciando aplicação ponto");
+
+        System.out.println("Ponto 2D");
+        Ponto2D ponto2D = new Ponto2D(UUID.randomUUID(), toBigDecimal(2), toBigDecimal(3));
+        System.out.println(ponto2D);
+
+        System.out.println("Ponto 3D");
+        Ponto3D ponto3D = new Ponto3D(UUID.randomUUID(), toBigDecimal(3), toBigDecimal(3), toBigDecimal(5));
+        System.out.println(ponto3D);
+    }
+}
