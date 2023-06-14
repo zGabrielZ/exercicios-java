e0f29f2cf9e3e759be703dd76f548ad3a47213ef
Gabriel Ferreira <ferreiragabriel2612@gmail.com>
2023-06-14 16:32:06 -0300

    Ajustes aplicacao fila e pilha

--- colecao/src/main/java/br/com/gabrielferreira/main/AplicacaoFila.java
+++ colecao/src/main/java/br/com/gabrielferreira/main/AplicacaoFila.java
@@ -3,23 +3,27 @@ package br.com.gabrielferreira.main;
 import br.com.gabrielferreira.model.Colecao;
 import br.com.gabrielferreira.model.Fila;
 import lombok.Generated;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Generated
 public class AplicacaoFila {
 
     public static void main(String[] args) {
-        System.out.println("Iniciando aplicação fila");
-
         Colecao colecao = new Fila();
         Fila fila = (Fila) colecao;
 
         statusFila(fila);
 
         System.out.println("Colocando alguns valores");
-        colecao.inserirItem(1);
-        colecao.inserirItem(4);
-        colecao.inserirItem(5);
-        colecao.inserirItem(10);
+        try {
+            colecao.inserirItem(1);
+            colecao.inserirItem(4);
+            colecao.inserirItem(5);
+            colecao.inserirItem(10);
+        } catch (Exception e){
+            log.warn("Ocorreu um erro ao inserir item na fila. Causa {}", e.getMessage());
+        }
         System.out.println("1, 4, 5, 10");
 
         statusFila(fila);
@@ -39,7 +43,6 @@ public class AplicacaoFila {
         for (Integer dado : fila.getStatusAtual()) {
             System.out.print(dado + ", ");
         }
-        System.out.println();
         System.out.println("**********************************");
     }
 }
--- colecao/src/main/java/br/com/gabrielferreira/main/AplicacaoPilha.java
+++ colecao/src/main/java/br/com/gabrielferreira/main/AplicacaoPilha.java
@@ -3,23 +3,27 @@ package br.com.gabrielferreira.main;
 import br.com.gabrielferreira.model.Colecao;
 import br.com.gabrielferreira.model.Pilha;
 import lombok.Generated;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Generated
 public class AplicacaoPilha {
 
     public static void main(String[] args) {
-        System.out.println("Iniciando aplicação pilha");
-
         Colecao colecao = new Pilha();
         Pilha pilha = (Pilha) colecao;
 
         statusPilha(pilha);
 
         System.out.println("Colocando alguns valores");
-        colecao.inserirItem(1);
-        colecao.inserirItem(4);
-        colecao.inserirItem(5);
-        colecao.inserirItem(10);
+        try {
+            colecao.inserirItem(1);
+            colecao.inserirItem(4);
+            colecao.inserirItem(5);
+            colecao.inserirItem(10);
+        } catch (Exception e){
+            log.warn("Ocorreu um erro ao inserir item na pilha. Causa {}", e.getMessage());
+        }
         System.out.println("1, 4, 5, 10");
 
         statusPilha(pilha);
@@ -40,7 +44,6 @@ public class AplicacaoPilha {
         for (Integer dado : pilha.getStatusAtual()) {
             System.out.print(dado + ", ");
         }
-        System.out.println();
         System.out.println("**********************************");
     }
 }
