e528728993161b94b4b5a93dd5871f5dd656c326
Gabriel Ferreira <ferreiragabriel2612@gmail.com>
2023-06-14 16:06:52 -0300

    Ajustes calculador service area

--- area/src/main/java/br/com/gabrielferreira/service/CalculadorService.java
+++ area/src/main/java/br/com/gabrielferreira/service/CalculadorService.java
@@ -1,5 +1,6 @@
 package br.com.gabrielferreira.service;
 
+import br.com.gabrielferreira.exception.RegraDeNegocioException;
 import br.com.gabrielferreira.model.AreaCalculavel;
 import br.com.gabrielferreira.model.FiguraComplexa;
 
@@ -15,7 +16,20 @@ public class CalculadorService implements Serializable {
     private static final long serialVersionUID = 6730307453958143191L;
 
     public BigDecimal somarCalculoAreas(List<AreaCalculavel> areas){
+        validarAreas(areas);
         FiguraComplexa figuraComplexa = new FiguraComplexa(UUID.randomUUID(), areas);
         return figuraComplexa.calcularArea();
+    }
+
+    private void validarAreas(List<AreaCalculavel> areas){
+        if(areas == null || areas.isEmpty()){
+            throw new RegraDeNegocioException("É necessário informar pelo menos uma área");
+        }
+
+        areas.forEach(areaCalculavel -> {
+            if(areaCalculavel == null){
+                throw new RegraDeNegocioException("É necessário informar área");
+            }
+        });
     }
 }
