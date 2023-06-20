99733cbe8fc9c3645b191d43551c8a570c46a6c8
Gabriel Ferreira <ferreiragabriel2612@gmail.com>
2023-06-20 20:59:44 -0300

    Ajustes projeto matematica

---
+++ matematica/src/main/java/br/com/gabrielferreira/model/enumeration/TipoAngulo.java
@@ -0,0 +1,20 @@
+package br.com.gabrielferreira.model.enumeration;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum TipoAngulo {
+
+    GRAUS,
+    RADIANOS;
+
+    public static boolean isGraus(TipoAngulo tipoAngulo){
+        return tipoAngulo.name().equals(GRAUS.name());
+    }
+
+    public static boolean isRadianos(TipoAngulo tipoAngulo){
+        return tipoAngulo.name().equals(RADIANOS.name());
+    }
+}
