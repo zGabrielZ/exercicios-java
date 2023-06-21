346f6e5c259ddece02dedf3139f6d54aefd138a2
Gabriel Ferreira <ferreiragabriel2612@gmail.com>
2023-06-20 21:05:28 -0300

    Ajustes projeto numeros primos

---
+++ numeros-primos/src/main/java/br/com/gabrielferreira/main/AplicacaoNumeroPrimo.java
@@ -0,0 +1,20 @@
+package br.com.gabrielferreira.main;
+
+import br.com.gabrielferreira.model.NumeroPrimo;
+import lombok.Generated;
+
+import java.util.UUID;
+
+@Generated
+public class AplicacaoNumeroPrimo {
+
+    public static void main(String[] args) {
+        System.out.println("Iniciando número primo");
+
+        NumeroPrimo numeroPrimo1 = new NumeroPrimo(UUID.randomUUID(), 2);
+        System.out.println("Valor " + numeroPrimo1.getValor() + " é primo ? " + (numeroPrimo1.isNumeroPrimo() ? "Sim" : "Não"));
+
+        NumeroPrimo numeroPrimo2 = new NumeroPrimo(UUID.randomUUID(), 11);
+        System.out.println("Valor " + numeroPrimo2.getValor() + " é primo ? " + (numeroPrimo2.isNumeroPrimo() ? "Sim" : "Não"));
+    }
+}
