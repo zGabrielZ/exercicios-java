040992a28e65c8b2b653fe068aa4a4da8dd4dec2
Gabriel Ferreira <ferreiragabriel2612@gmail.com>
2023-06-20 21:14:07 -0300

    Ajustes projeto ponto

---
+++ ponto/src/main/java/br/com/gabrielferreira/model/Ponto2D.java
@@ -0,0 +1,33 @@
+package br.com.gabrielferreira.model;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Ponto2D implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 537563925647530667L;
+
+    @Getter
+    @EqualsAndHashCode.Include
+    private UUID id;
+
+    @Getter
+    private BigDecimal coordX;
+
+    @Getter
+    private BigDecimal coordY;
+
+    @Override
+    public String toString() {
+        return String.format("Id : %s, coordX : %s, coordY : %s", id, coordX, coordY);
+    }
+}
