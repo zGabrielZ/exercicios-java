684180e6ac4398fa3069c21d87ad1ea10d2f2d1f
Gabriel Ferreira <ferreiragabriel2612@gmail.com>
2023-06-26 20:58:51 -0300

    Projeto circ pi volume exercicio

---
+++ circunferencia-volume-pi/src/main/java/br/com/gabrielferreira/main/AplicacaoCircunferenciaVolumePi.java
@@ -0,0 +1,37 @@
+package br.com.gabrielferreira.main;
+import lombok.Generated;
+import lombok.extern.slf4j.Slf4j;
+import java.util.Locale;
+import java.util.Scanner;
+
+import static br.com.gabrielferreira.utils.MascarasUtils.valorFormatadoBrasil;
+import static br.com.gabrielferreira.validate.ValidarEntrada.validarEntradaDouble;
+import static br.com.gabrielferreira.utils.CalcularRaioUtils.*;
+import static br.com.gabrielferreira.utils.CalculoUtils.*;
+
+@Slf4j
+@Generated
+public class AplicacaoCircunferenciaVolumePi {
+
+    public static void main(String[] args) {
+        Locale locale = new Locale("pt", "BR");
+        Scanner scanner = new Scanner(System.in);
+        scanner.useLocale(locale);
+
+        try {
+            System.out.println("Digite o raio : ");
+            Double valor = validarEntradaDouble(scanner);
+
+            Double circuferencia = calcularCircunferencia(valor);
+            Double volume = calcularVolume(valor);
+
+            System.out.println("Circunferência : " + valorFormatadoBrasil(toBigDecimal(circuferencia)));
+            System.out.println("Volume : " + valorFormatadoBrasil(toBigDecimal(volume)));
+            System.out.println("PI : " + valorFormatadoBrasil(toBigDecimal(PI)));
+        } catch (Exception e){
+            log.warn("Ocorreu erro na aplicação. Causa : {}", e.getMessage());
+        }
+
+        scanner.close();
+    }
+}
