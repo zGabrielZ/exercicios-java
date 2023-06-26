16b625e4a0f451a3acb1f3c485110e5cae547c3f
Gabriel Ferreira <ferreiragabriel2612@gmail.com>
2023-06-26 20:29:10 -0300

    Projeto funcionario exercicio

---
+++ funcionario/src/main/java/br/com/gabrielferreira/main/AplicacaoFuncionario.java
@@ -0,0 +1,48 @@
+package br.com.gabrielferreira.main;
+import br.com.gabrielferreira.model.Funcionario;
+import br.com.gabrielferreira.service.FuncionarioService;
+import lombok.Generated;
+import lombok.extern.slf4j.Slf4j;
+
+import java.math.BigDecimal;
+import java.util.Locale;
+import java.util.Scanner;
+import static br.com.gabrielferreira.validate.ValidarEntrada.validarEntradaBigDecimal;
+
+@Slf4j
+@Generated
+public class AplicacaoFuncionario {
+
+    public static void main(String[] args) {
+        Locale locale = new Locale("pt", "BR");
+        Scanner scanner = new Scanner(System.in);
+        scanner.useLocale(locale);
+
+        FuncionarioService funcionarioService = new FuncionarioService();
+
+        try {
+            System.out.println("Nome : ");
+            String nome = scanner.nextLine();
+
+            System.out.println("Salário bruto : ");
+            BigDecimal salarioBruto = validarEntradaBigDecimal(scanner);
+
+            System.out.println("Taxa : ");
+            BigDecimal taxa = validarEntradaBigDecimal(scanner);
+
+            Funcionario funcionario = funcionarioService.criarFuncionario(nome, salarioBruto, taxa);
+            System.out.println(funcionarioService.imprimirFuncionario(funcionario));
+
+            System.out.println("Qual a porcentagem para adicionar no salário do funcionário ? ");
+            BigDecimal porcentagem = validarEntradaBigDecimal(scanner);
+
+            funcionarioService.adicionarPorcentagemSalario(funcionario, porcentagem);
+            System.out.println(funcionarioService.imprimirFuncionario(funcionario));
+
+        } catch (Exception e){
+            log.warn("Ocorreu erro na aplicação. Causa : {}", e.getMessage());
+        }
+
+        scanner.close();
+    }
+}
---
+++ funcionario/src/main/java/br/com/gabrielferreira/service/FuncionarioService.java
@@ -0,0 +1,88 @@
+package br.com.gabrielferreira.service;
+
+import br.com.gabrielferreira.exception.RegraDeNegocioException;
+import br.com.gabrielferreira.model.Funcionario;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.UUID;
+
+import static br.com.gabrielferreira.utils.CalculoUtils.*;
+import static br.com.gabrielferreira.utils.MascarasUtils.*;
+
+public class FuncionarioService implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 537563925647530667L;
+
+    public Funcionario criarFuncionario(String nome, BigDecimal salarioBruto, BigDecimal taxa){
+        validarNome(nome);
+        validarValorInformado(salarioBruto, "Salário Bruto");
+        validarValorInformado(taxa, "Taxa");
+        validarTaxaComSalarioBruto(salarioBruto, taxa);
+
+        return Funcionario.builder()
+                .id(UUID.randomUUID())
+                .nome(nome)
+                .salarioBruto(salarioBruto)
+                .taxa(taxa)
+                .salarioLiquido(subtrair(salarioBruto, taxa))
+                .build();
+    }
+
+    public void adicionarPorcentagemSalario(Funcionario funcionario, BigDecimal porcentagem){
+        verificarFuncionario(funcionario);
+        validarPorcentagem(porcentagem);
+
+        BigDecimal porcentagemCalculada = divide(porcentagem, toBigDecimal(100), RoundingMode.HALF_EVEN);
+        funcionario.setSalarioLiquido(somar(funcionario.getSalarioLiquido(), multiplicar(funcionario.getSalarioBruto(), porcentagemCalculada)));
+    }
+
+    public String imprimirFuncionario(Funcionario funcionario){
+        verificarFuncionario(funcionario);
+
+        return "Nome do funcionário : " +
+                funcionario.getNome() +
+                ", " + valorMonetarioBrasil(funcionario.getSalarioLiquido());
+    }
+
+    private void validarNome(String nome){
+        if(nome == null || nome.isBlank()){
+            throw new RegraDeNegocioException("É necessário informar o nome");
+        }
+    }
+
+    private void validarValorInformado(BigDecimal valor, String msg){
+        if(valor == null){
+            throw new RegraDeNegocioException(String.format("É necessário informar %s", msg));
+        }
+
+        if(valor.compareTo(BigDecimal.ZERO) < 0){
+            throw new RegraDeNegocioException(String.format("%s não pode ser negativo", msg));
+        }
+    }
+
+    private void validarTaxaComSalarioBruto(BigDecimal salarioBruto, BigDecimal taxa){
+        if(salarioBruto.compareTo(taxa) < 0){
+            throw new RegraDeNegocioException("Salário bruto não pode ser menor que a taxa");
+        }
+    }
+
+    private void verificarFuncionario(Funcionario funcionario){
+        if(funcionario == null){
+            throw new RegraDeNegocioException("É necessário informar funcionário");
+        }
+    }
+
+    private void validarPorcentagem(BigDecimal porcentagem){
+        if(porcentagem == null){
+            throw new RegraDeNegocioException("É necessário informar a porcentagem");
+        }
+
+        if(porcentagem.compareTo(BigDecimal.ZERO) < 0){
+            throw new RegraDeNegocioException("A porcentagem não pode ser negativa");
+        }
+    }
+}
