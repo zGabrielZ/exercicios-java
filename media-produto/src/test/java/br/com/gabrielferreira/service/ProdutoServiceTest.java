cf3dbc4fb4fddf8f6b5019497560d4a823bb6653
Gabriel Ferreira <ferreiragabriel2612@gmail.com>
2023-07-03 20:35:23 -0300

    Projeto media produto exercicio

---
+++ media-produto/src/test/java/br/com/gabrielferreira/service/ProdutoServiceTest.java
@@ -0,0 +1,77 @@
+package br.com.gabrielferreira.service;
+
+import br.com.gabrielferreira.exception.RegraDeNegocioException;
+import br.com.gabrielferreira.model.Produto;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static br.com.gabrielferreira.utils.CalculoUtils.*;
+
+class ProdutoServiceTest {
+
+    private ProdutoService produtoService;
+
+    @BeforeEach
+    public void criarInstancias(){
+        produtoService = new ProdutoService();
+    }
+
+    @Test
+    @DisplayName("Deve validar criação do produto quando informar o nome nulo")
+    void deveValidarCriarProdutoNomeNulo(){
+        String nome = null;
+        BigDecimal preco = toBigDecimal(10.0);
+        assertThrows(RegraDeNegocioException.class, () -> produtoService.criarProduto(nome, preco));
+    }
+
+    @Test
+    @DisplayName("Deve validar criação do produto quando informar o preço nulo")
+    void deveValidarCriarProdutoPrecoNulo(){
+        String nome = "Banana";
+        BigDecimal preco = null;
+        assertThrows(RegraDeNegocioException.class, () -> produtoService.criarProduto(nome, preco));
+    }
+
+    @Test
+    @DisplayName("Deve validar criação do produto quando informar o preço negativo")
+    void deveValidarCriarProdutoPrecoNegativo(){
+        String nome = "Banana";
+        BigDecimal preco = toBigDecimal(-10.0);
+        assertThrows(RegraDeNegocioException.class, () -> produtoService.criarProduto(nome, preco));
+    }
+
+    @Test
+    @DisplayName("Deve criar produto quando informar valores corretos")
+    void deveCriarProduto(){
+        Produto produto = produtoService.criarProduto("Banana", toBigDecimal(10.0));
+
+        assertNotNull(produto.getId());
+        assertEquals(toBigDecimal(10.0), produto.getPreco());
+    }
+
+    @Test
+    @DisplayName("Deve validar calculo média de preços quando informar nulo")
+    void deveValidarCalculoMediaPrecoProdutoQuandoInformarNulo(){
+        Produto[] produtos = new Produto[2];
+        produtos[0] = new Produto(UUID.randomUUID(), "Banana", toBigDecimal(10.0));
+        produtos[1] = null;
+        assertThrows(RegraDeNegocioException.class, () -> produtoService.calcularMediaProdutoPreco(produtos));
+    }
+
+    @Test
+    @DisplayName("Deve realizar calculo média de preçcos quando informar valores corretamentes")
+    void deveRealizarCalculoMediaPrecoProdutos(){
+        Produto[] produtos = new Produto[2];
+        produtos[0] = new Produto(UUID.randomUUID(), "Banana", toBigDecimal(10.0));
+        produtos[1] = new Produto(UUID.randomUUID(), "Limão", toBigDecimal(15.0));
+
+        BigDecimal media = produtoService.calcularMediaProdutoPreco(produtos);
+
+        assertEquals(toBigDecimal(12.5), media);
+    }
+}
