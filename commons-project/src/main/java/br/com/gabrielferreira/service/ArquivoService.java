c0dc7c279f87e4564fb0cf8d52fa0eef709f31ad
Gabriel Ferreira <ferreiragabriel2612@gmail.com>
2023-08-08 17:15:05 -0300

    ajustes

--- commons-project/src/main/java/br/com/gabrielferreira/service/ArquivoService.java
+++ commons-project/src/main/java/br/com/gabrielferreira/service/ArquivoService.java
@@ -22,7 +22,25 @@ public class ArquivoService implements Serializable {
 
     public String lerArquivo(String entrada){
         InputStream inputStream = buscarCaminho(entrada);
-        try(BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))){
+
+        if(inputStream == null){
+            return lerArquivoSemInput(entrada);
+        }
+
+        return lerArquivoComInput(inputStream);
+    }
+    public InputStream buscarCaminho(String entrada){
+        try {
+            ClassLoader classLoader = ArquivoService.class.getClassLoader();
+            return classLoader.getResourceAsStream(entrada);
+        } catch (Exception e){
+            gerarLogWarn("Ocorreu erro ao buscar arquivo. Causa : {}", e);
+            throw new ErroInesperadoException("Arquivo não encontrado");
+        }
+    }
+
+    private String lerArquivoSemInput(String entrada){
+        try(BufferedReader bufferedReader = new BufferedReader(new FileReader(entrada, StandardCharsets.UTF_8))){
 
             String lerArquivo;
             StringBuilder stringBuilder = new StringBuilder();
@@ -38,13 +56,22 @@ public class ArquivoService implements Serializable {
             throw new ErroInesperadoException("Ocorreu um erro ler o arquivo");
         }
     }
-    public InputStream buscarCaminho(String entrada){
-        try {
-            ClassLoader classLoader = ArquivoService.class.getClassLoader();
-            return classLoader.getResourceAsStream(entrada);
+
+    private String lerArquivoComInput(InputStream inputStream){
+        try(BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))){
+
+            String lerArquivo;
+            StringBuilder stringBuilder = new StringBuilder();
+
+            while ((lerArquivo = bufferedReader.readLine()) != null){
+                stringBuilder.append(lerArquivo).append("\n");
+            }
+
+            return stringBuilder.toString().trim();
+
         } catch (Exception e){
-            gerarLogWarn("Ocorreu erro ao buscar arquivo. Causa : {}", e);
-            throw new ErroInesperadoException("Arquivo não encontrado");
+            gerarLogWarn("Ocorreu erro ao ler o arquivo : {}", e);
+            throw new ErroInesperadoException("Ocorreu um erro ler o arquivo");
         }
     }
 }
